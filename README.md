c0c1e055b7a845afd7f7486a5d5cbd7472294805
Jed Kyung <jed@jiver.co>
2016-01-04 22:21:42 -0800

    initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# [Inteage](http://inteage.com) - The Simplest Solution for Chat and Messaging
+[Inteage](http://inteage.com) enables your app to engage and retain your users in real-time.
+
+## Documentation
+https://inteage.gitbooks.io/ios-sdk/content/en/index.html
+
+## Sample
+You can download [a sample app](https://github.com/smilefam/inteage-sample).
+
+## Other platforms
+* [Android](https://inteage.gitbooks.io/android-sdk/content/en/index.html)
+* [Unity3D](https://inteage.gitbooks.io/unity-sdk/content/en/index.html)
+* [Web(JavaScript)](https://inteage.gitbooks.io/inteage-web-doc/content/en/index.html)
+* [Server API](https://inteage.gitbooks.io/api/content/en/index.html)
---
+++ SendBirdSDK.framework/Headers/SendBirdSDK.h
@@ -0,0 +1,36 @@
+//
+//  SendBirdSDK.h
+//  SendBirdSDK
+//
+//  Created by SendBird Developers on 2015. 3. 1. in San Francisco, CA.
+//  Copyright (c) 2015 SENDBIRD.COM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SendBirdSDK.
+FOUNDATION_EXPORT double SendBirdSDKVersionNumber;
+
+//! Project version string for SendBirdSDK.
+FOUNDATION_EXPORT const unsigned char SendBirdSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SendBirdSDK/PublicHeader.h>
+
+#import <SendBirdSDK/SendBirdAPIClient.h>
+#import <SendBirdSDK/SendBirdChannelListQuery.h>
+#import <SendBirdSDK/SendBirdCommand.h>
+#import <SendBirdSDK/SendBird.h>
+#import <SendBirdSDK/SendBirdClient.h>
+#import <SendBirdSDK/SendBirdChannel.h>
+#import <SendBirdSDK/SendBirdFileInfo.h>
+#import <SendBirdSDK/SendBirdFileLink.h>
+#import <SendBirdSDK/SendBirdMessage.h>
+#import <SendBirdSDK/SendBirdSystemMessage.h>
+#import <SendBirdSDK/SendBirdSender.h>
+#import <SendBirdSDK/SendBirdWSClient.h>
+#import <SendBirdSDK/SendBirdBroadcastMessage.h>
+#import <SendBirdSDK/SendBirdMemberInMessagingChannel.h>
+#import <SendBirdSDK/SendBirdMember.h>
+#import <SendBirdSDK/SendBirdMessageListQuery.h>
+#import <SendBirdSDK/SendBirdMessagingUnreadCountQuery.h>
+#import <SendBirdSDK/SendBirdMessagingChannelUpdate.h>
