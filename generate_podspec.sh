2223bc3a6d6f7a8a051a85dd474c092944b05462
Celine Moon <peaceluverceline@gmail.com>
2023-04-26 23:07:02 +0900

    Update podspec script.

--- generate_podspec.sh
+++ generate_podspec.sh
@@ -27,11 +27,12 @@ Pod::Spec.new do |s|
   s.authors      = {
     'Jed Gyeong' => 'jed.gyeong@sendbird.com',
     'Celine Moon' => 'celine.moon@sendbird.com',
-    'Erenst Hong' => 'ernest.hong@sendbird.com'
+    'Ernest Hong' => 'ernest.hong@sendbird.com'
+    'Damon Park' => 'damon.park@sendbird.com'
   }
   s.source       = { :git => 'https://github.com/sendbird/sendbird-ios-framework.git', :tag => \"v$VERSION\" }
   s.requires_arc = true
-  s.platform = :ios, '9.0'
+  s.platform = :ios, '11.0'
   s.documentation_url = 'https://docs.sendbird.com/'
   s.ios.vendored_frameworks = 'Sources/SendBirdSDK.xcframework'
   s.ios.frameworks = ['UIKit', 'CFNetwork', 'Security', 'Foundation', 'MobileCoreServices', 'SystemConfiguration', 'CoreFoundation']
